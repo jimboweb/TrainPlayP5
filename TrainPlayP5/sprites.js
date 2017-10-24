6cf6295bf72cfaaef8f57d5db0c48ea4fd7210c4
jimboweb <jimboweb@gmail.com>
2017-10-23 22:50:28 -0400

    First commit

---
+++ TrainPlayP5/sprites.js
@@ -0,0 +1,23 @@
+class Train{
+    //Sprite class
+    //draw function = train image]
+    //Contains go to stations command
+    //Constructor(var world)
+}
+
+class Station{
+    //create sprite
+    //draw function is a circle
+    //    also draw line to connected stations
+    //boolean isFinalStation command
+    //boolean hidden
+    //visited function
+    //    reveals connected stations
+    //    changes color
+    //    reports if you won the game
+    //Constructor(var world)
+    //draw function only if not hidden
+    var show =function(){
+        //turn off hidden
+    }
+}
---
+++ TrainPlayP5/world.js
@@ -0,0 +1,18 @@
+class TrainWorld{
+
+    var stations = [];
+    var locations = [];
+    var train;
+    //constructor
+    //push locations of all stations
+    //push all the stations
+
+    var drawWorld = function(){
+        //place all the stations
+        //place the train
+    }
+}
+
+//instantiate TrainWorld
+//instantiate stations
+//instantiate trains
